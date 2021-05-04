fe684968afda6b1cadfe90a64d5129fd8774f475
Noah Hoback <fullstack47@outlook.com>
2021-05-04 15:09:35 -0400

    adding functions and more constants for server.js

--- server.js
+++ server.js
@@ -6,3 +6,29 @@ const { json } = require("express");
 const fs = require("fs");
 const db = `${__dirname}/db/db.json`
 let notes = [];
+
+// functions
+
+const writeNotes = () => {
+    fs.writeFileSync(db, JSON.stringify(notes), "utf8");
+};
+const deleteNotes = (id) => {
+    notes.splice(
+        notes.findIndex((note) => note.id == id),
+        1
+    );
+    writeNotes();
+    return notes;
+};
+const getNotes = () => {
+    const temp = fs.readFileSync(db, "utf8", (err, data) => {
+        if (err) throw err;
+        const updateNotes = [];
+        JSON.parse(data).foreach((note) => {
+            updateNotes.push(note);
+        });
+        return updateNotes;
+    });
+    notes = JSON.parse(temp);
+    return notes;
+};
