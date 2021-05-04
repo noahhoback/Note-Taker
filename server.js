6c8044635d3a592d8d4994950a10b772eed87e90
Noah Hoback <fullstack47@outlook.com>
2021-05-04 15:29:14 -0400

    added routing to server.js

--- server.js
+++ server.js
@@ -32,3 +32,46 @@ const getNotes = () => {
     notes = JSON.parse(temp);
     return notes;
 };
+
+//IDs
+const generateIDs = () => {
+    const currentIDs = [];
+    notes.forEach((note) => currentIDs.push(note.id));
+
+    let newID = "";
+    do {
+        newID = Math.floor(Math.random() * 10000 + 1).toString();
+    } while (currentIDs.includes(newID));
+    return newID;
+    };
+
+//Routing
+//show notes
+app.get("/api/notes", (req, res) => {
+    return res.json(getNotes());
+});
+//delete notes
+app.delete("/api/notes/:id", (req, res) => {
+    return res.json(deleteNotes(req.params.id));
+});
+// save the note
+app.post("/api/notes", (req, res) => {
+    return res.json(saveNotes(req.body));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "publick/notes.html"));
+});
+
+//home page
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+getNotes();
+
+//server to begin listening
+app.listen(PORT, () => {
+    console.log("The application is listening on PORT " + PORT);
+});
+
