10db5bb8a5f1681d8ede7d512c7837d78b6f42ab
Noah Hoback <fullstack47@outlook.com>
2021-05-04 16:17:22 -0400

    adding code to display current note

--- index.js
+++ index.js
@@ -4,9 +4,41 @@ const $saveNote = $(".save-note");
 const $newNote = $(".new-note");
 const $noteContent = $(".note-textarea");
 
+let currentNote = {};
+
 const getNotes = () => {
     return $.ajax({
         url: "/api/notes",
         method: "GET",
     });
+};
+
+const saveNotes = (note) => {
+    return $.ajax({
+        url: "/api/notes",
+        data: note,
+        method: "POST",
+    });
+};
+
+const deleteNote = (id) => {
+    return $.ajax({
+        url: "api/notes/" + id,
+        method: "DELETE",
+    });
+};
+
+const getCurrentNote = () => {
+    $saveNote.hide();
+    if (currentNote.id) {
+        $noteTitle.attr("readonly", true);
+        $noteContent.attr("readonly", true);
+        $noteTitle.val(currentNote.title);
+        $noteContent.val(currentNote.text);
+    } else {
+        $noteTitle.attr("readonly", false);
+        $noteContent.attr("readonly", false);
+        $noteTitle.val("");
+        $noteContent.val("");
+    }
 };
